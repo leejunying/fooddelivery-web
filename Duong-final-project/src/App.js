f8fc8dd56afff4b94d0be1a0ffce27c6fb8a4973
dpham4 <dpham4@binghamton.edu>
2021-10-06 22:17:10 +0700

    Update banner for home

--- Duong-final-project/src/App.js
+++ Duong-final-project/src/App.js
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Banner from './components/Banner';
 function App() {
   return (
     <div>
       <Navbar/>
+      <Banner/>
       <Footer/>
     </div>
   );
--- Duong-final-project/src/components/Banner.js
+++ Duong-final-project/src/components/Banner.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import image from "./banner-image.jpg";
+import './Banner.css'
 function Banner() {
     return (
-        <div>
-            
+        <div className = 'banner'>
+            <div className = 'banner__left'>
+                <h1>Safe food</h1>
+                <h1>Delivery</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel alias maxime consectetur. Ad iusto vitae nesciunt sunt dolor Amet</p>
+                <button>ORDER NOW</button>
+            </div>
+            <img src={image} alt='banner'/>            
         </div>
     )
 }
