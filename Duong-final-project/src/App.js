a8c91c022186970ff04239345f3082aeec9c5ca5
dpham4 <dpham4@binghamton.edu>
2021-10-07 22:43:00 +0700

    Update App.css with common style

--- Duong-final-project/src/App.js
+++ Duong-final-project/src/App.js
@@ -1,18 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Banner from './components/Banner';
-import Menu from './components/Menu';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Menu from "./components/Menu";
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Banner/>
-      <Menu/>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Route path="/">
+          <Navbar />
+          <Banner />
+          <Menu />
+          <Footer />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
--- Duong-final-project/src/components/Navbar.js
+++ Duong-final-project/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 import './Navbar.css';
 function Navbar() {
@@ -7,9 +7,9 @@ function Navbar() {
     <div className = 'navbar__main'>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className='navbar-brand' to='/'>
             FoodHub
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
