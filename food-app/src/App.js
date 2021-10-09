9ab34fe570f18bc87131607ceb74921f5532eb24
dpham4 <54454479+dpham4@users.noreply.github.com>
2021-10-10 00:19:58 +0700

    Update new components

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Main from "./Routers/Main";
-
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 function App() {
   const Loadcomponents = (routes) => {
     var result = [];
@@ -25,12 +25,22 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <Header></Header>
-        <Switch>{Loadcomponents(Main)}</Switch>
-        <Footer></Footer>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Header/>
+            <Footer/>
+          </Route>
+        </Switch>
       </div>
     </Router>
+    // <Router path = '/'>
+    //   <div className="App">
+    //     <Header></Header>
+    //     <Switch>{Loadcomponents(Main)}</Switch>
+    //     <Footer></Footer>
+    //   </div>
+    // </Router>
   );
 }
 
